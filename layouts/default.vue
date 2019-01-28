0f135855c7643241aba31943a5ff553bf379fbba
Matthieu Marais <mqtthieu@gmail.com>
2019-01-28 23:06:21 +0100

    toggle menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,19 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <nuxt-link class="navbar-brand" to="/"><img src="~assets/img/logoHand.png" alt=""></nuxt-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button @click="toggleMenu()" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" v-bind:class="{show : isOpen}">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
+        <li class="nav-item" @click="toggleMenu()">
           <nuxt-link class="nav-link" to="/">Home</nuxt-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" @click="toggleMenu()">
           <nuxt-link class="nav-link" to="/actus">Actualités</nuxt-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" @click="toggleMenu()">
           <nuxt-link class="nav-link" to="/contact">Contact</nuxt-link>
         </li>
       </ul>
@@ -25,6 +25,16 @@
 </template>
 <script>
 export default {
+  data(){
+    return{
+      isOpen : false
+    }
+  },
+  methods : {
+    toggleMenu(){
+      this.isOpen = !this.isOpen
+    }
+  }
   
 }
 </script>
